6850776478820cd8e597bba5818a332f3f66ffef
John Torres <jtorres0802@gmail.com>
2019-07-23 10:20:52 -0400

    Uploaded numeric conversion tests.

---
+++ Mod2/MyWork/NumericConversions/conversionTests.js
@@ -0,0 +1,22 @@
+// Conversion Tests
+// by John Torres
+
+var a = "  101.1  ";
+var b = "402 Stevens";
+var c = "Number 5  ";
+var d = "55";
+
+console.log(parseInt(a)); // Test Parse Ints
+console.log(parseInt(b));
+console.log(parseInt(c));
+console.log(parseInt(d));
+
+console.log(parseFloat(a)); // Test Parse Floats
+console.log(parseFloat(b));
+console.log(parseFloat(c));
+console.log(parseFloat(d));
+
+console.log(Number(a)); // Test convert to Number
+console.log(Number(b));
+console.log(Number(c));
+console.log(Number(d));
