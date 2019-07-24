5895c19b82d1176558ad3792c26d3e8775e491ed
John Torres <jtorres0802@gmail.com>
2019-07-24 08:29:17 -0400

    Tinkered with DOM manipulation.

--- Mod3/MyWork/ifScripts/gregorianCalendar.js
+++ Mod3/MyWork/ifScripts/gregorianCalendar.js
@@ -2,11 +2,13 @@
 // by John Torres
 "use strict"
 
-// User Inputs
-var testYear = 2000;
+// User Input
+var testYear = 2001;
 
 if (testYear % 4 == 0 && (testYear % 100 != 0 || testYear % 400 == 0)) {
+    document.getElementById("answer").innerHTML = testYear + " is a leap year!";
     console.log(testYear + " is a leap year!");
 } else {
+    document.getElementById("answer").innerHTML = testYear + " is not a leap year.";
     console.log(testYear + " is not a leap year.");
 }
