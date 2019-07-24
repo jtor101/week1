12d141fdd125a0926a04d7a91078f6969ebf7a7a
John Torres <jtorres0802@gmail.com>
2019-07-24 15:12:08 -0400

    Added tax functions.

--- Mod4/MyWork/ReturningValuesScripts/taxFunctions.js
+++ Mod4/MyWork/ReturningValuesScripts/taxFunctions.js
@@ -17,22 +17,23 @@ function getMedicareTax(grossPay) {
 
 // getFederalTax
 function getFederalTax(grossPay, withholdingCode) {
+    var federalTax;
+    var fedTaxRt;
     if (withholdingCode == 0) {
-        var federalTax = grossPay * .23;
-        return federalTax;
+        fedTaxRt = .23;
     } else if (withholdingCode == 1) {
-        var federalTax = grossPay * .21;
-        return federalTax;
+        fedTaxRt = .21;
     } else if (withholdingCode == 2) {
-        var federalTax = grossPay * .195;
-        return federalTax;
+        fedTaxRt = .195;
     } else if (withholdingCode == 3) {
-        var federalTax = grossPay * .185;
-        return federalTax;
+        fedTaxRt = .185;
+    } else if (withholdingCode >= 40) {
+        fedTaxRt = 0;
     } else {
-        var federalTax = grossPay * (.18 - ((withholdingCode - 4) * .005));
-        return federalTax;
+        fedTaxRt = .18 - ((withholdingCode - 4) * .005);
     }
+    federalTax = grossPay * fedTaxRt;
+    return federalTax;
 }
 
 // User Inputs
@@ -47,4 +48,10 @@ var myFederalTax = getFederalTax(myGrossPay, myWithholdingCode);
 // Console Output
 console.log("The Social Security tax on a gross pay of $" + myGrossPay + " is $" + mySocSecTax.toFixed(2));
 console.log("The Medicare tax on a gross pay of $" + myGrossPay + " is $" + myMedicareTax.toFixed(2));
-console.log("The Federal tax on a gross pay of $" + myGrossPay + " with a withholding code of " + myWithholdingCode + " is $" + myFederalTax.toFixed(2));
+if (myWithholdingCode >= 0) {
+    console.log("The Federal tax on a gross pay of $" + myGrossPay +
+        " with a withholding code of " + myWithholdingCode +
+        " is $" + myFederalTax.toFixed(2));
+} else {
+    console.log("Please enter a withholding code of 0 or greater.");
+}
